468befcfa6d1a033415df2ff339ad97a15aa7217
Nicolás Pinochet <nicolas.apf@outlook.com>
2021-09-12 03:21:42 -0300

    media-mediana-moda

---
+++ curso-Python/estadistica.py
@@ -0,0 +1,21 @@
+import math
+import statistics as stats
+
+datos = []
+contRelleno = 1
+cont = 0
+num = int(input("\t --> ¿Cuantos digitos piensa ingresar? <-- \n limite: "))
+
+while contRelleno == 1:   
+    dato = int(input("Digite un numero para el arreglo: "))
+    datos.append(dato)
+    cont += 1
+    if cont == num:
+        print(datos)
+        print("\t Media")
+        print(f"la media de los datos ingresados es: {stats.harmonic_mean(datos):.2f}")
+        print("\t Moda")
+        print(f"La Moda de los datos ingresados es: {stats.mode(datos)}")
+        print("\t Mediana")
+        print(f"La mediana de los datos es: {stats.median(datos)}")
+        contRelleno = 0
